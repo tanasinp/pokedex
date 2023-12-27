1d2e97693ea04b93d42f36607ddf60e6210048ac
tanasinp <wavet888@gmail.com>
2023-12-27 13:23:19 +0700

    search button

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
 import './App.css'
+import { useGetAllPokemonQuery, } from './services/pokemon' 
 
 //import components
 import Header from "./components/Header";
+import LiveSearch from './components/LiveSearch';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, error, isLoading } = useGetAllPokemonQuery() 
+  const [selectedPoke, setSelectedPoke] = useState(null);
+
+  if (error) {
+    return <>Oh no, there was an error: {error.message}</>;
+  }
+
+  if (isLoading) {
+    return <>Loading...</>;
+  }
 
   return (
     <div>
       <div className="AppHeader flex items-center justify-center">
         <Header/>
+      </div>
+      <div>
+        <p>Selected Pokemon : {selectedPoke ? selectedPoke.name : 'None'}  </p>
+      </div>
+      <div>
+        <LiveSearch setSelectedPoke={setSelectedPoke}/>
       </div>
     </div>
   )
---
+++ src/components/LiveSearch.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { useGetAllPokemonQuery } from '../services/pokemon'
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import { Box } from '@mui/material';
+import Stack from '@mui/material/Stack';
+
+function LiveSearch({ selectedPoke, setSelectedPoke }) {
+  const { data, error, isLoading } = useGetAllPokemonQuery()
+
+  const handleAutocompleteChange = (event, value) => {
+    if (value) {
+      console.log('Selected Pokemon:', value.name);
+      setSelectedPoke(value); // Set the selected value using setSelectedPoke
+    }
+  };
+
+  return (
+    <Stack sx={{width:300,margin: "auto"}}>
+      <Autocomplete
+        id="poke-box-demo"
+        getOptionLabel = {(option) => option.name}
+        options = {data.results}
+        sx={{ width: 300 }}
+        noOptionsText={"I don't have this pokemon."}
+        renderOption={(props,option) => (
+          <Box component="li" {...props} key={option.id}>
+            {option.name}
+          </Box>
+        )}
+        renderInput={(params) => <TextField {...params} label="Pokemon name"  />}
+        onChange={handleAutocompleteChange}
+      />
+    </Stack>
+  )
+}
+
+export default LiveSearch
