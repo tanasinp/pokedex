a8d94bb675b69a4b0564cf9809f85232b152d91a
tanasinp <wavet888@gmail.com>
2023-12-28 13:32:46 +0700

    taiwind

--- src/App.jsx
+++ src/App.jsx
@@ -20,15 +20,29 @@ function App() {
 
   return (
     <div>
+      
       <div className="AppHeader flex items-center justify-center">
         <Header/>
       </div>
-      <div>
+      <div className='Nav'>
         <p>Selected Pokemon : {selectedPoke ? selectedPoke.name : 'None'}  </p>
       </div>
-      <div>
+      <div className="searchBar">
         <LiveSearch setSelectedPoke={setSelectedPoke}/>
       </div>
+      
+      <div className='max-w-5xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 '>
+        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
+          <div>
+             <h2>1</h2>
+          </div>
+          <div>
+            <h2>1</h2>
+          </div>
+        </div>
+      </div>
+      
+      
     </div>
   )
 }
