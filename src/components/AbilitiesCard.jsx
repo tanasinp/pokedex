a57b776a50596fa22ee1b10778176e4ff2f964ba
tanasinp <wavet888@gmail.com>
2023-12-28 20:51:21 +0700

    .

---
+++ src/components/AbilitiesCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useGetPokemonByNameQuery } from '../services/pokemon';
+
+function AbilitiesCard() {
+  return (
+    <div>AbilitiesCard</div>
+  )
+}
+
+export default AbilitiesCard
