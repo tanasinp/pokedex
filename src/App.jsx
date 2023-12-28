3b9aa86337c5a3dfd7d13201cafab1f9d79f5ca3
tanasinp <wavet888@gmail.com>
2023-12-28 13:38:13 +0700

    shiny button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css'
 import { useGetAllPokemonQuery, } from './services/pokemon' 
+import { FaStar,FaRegStar } from "react-icons/fa";
 
 //import components
 import Header from "./components/Header";
@@ -9,6 +10,11 @@ import LiveSearch from './components/LiveSearch';
 function App() {
   const { data, error, isLoading } = useGetAllPokemonQuery() 
   const [selectedPoke, setSelectedPoke] = useState(null);
+  const [shiny, setShiny] = useState(false); 
+
+  const toggleShiny = () => {
+    setShiny((prevShiny) => !prevShiny); 
+  };
 
   if (error) {
     return <>Oh no, there was an error: {error.message}</>;
@@ -29,6 +35,11 @@ function App() {
       </div>
       <div className="searchBar">
         <LiveSearch setSelectedPoke={setSelectedPoke}/>
+      </div>
+      <div className='shinyButton'>
+        <button onClick={toggleShiny}>
+          {shiny ? <FaStar /> : <FaRegStar />}
+        </button>
       </div>
       
       <div className='max-w-5xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 '>
